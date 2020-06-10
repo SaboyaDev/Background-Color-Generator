bc989e0ced30d953cb208a2c70e2ef7e426a1f2e
joseluissaboya <joseluissaboya@gmail.com>
2020-06-10 01:07:08 -0400

    completed the change of color with js and display the color as the colors get changed by the user

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 let color1 = document.querySelector('#color1');
 let color2 = document.querySelector('#color2');
 let body = document.querySelector('body');
+let css = document.querySelector('#colorDisplay');
+let h3 = document.querySelector('h3');
 
-color1.addEventListener('input', () => {
+function setGradient() {
 	body.style.background =
 		'linear-gradient(to right, ' + color1.value + ',' + color2.value + ')';
-});
+  h3.textContent = body.style.backgroundImage;
+}
 
-color2.addEventListener('input', () => {
-	body.style.background =
-		'linear-gradient(to right, ' + color1.value + ',' + color2.value + ')';
-});
+color1.addEventListener('input', setGradient);
+
+color2.addEventListener('input', setGradient);
